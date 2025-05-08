b15ff389dcbbfc4c940f4bca1fba9c96ab7a4eea
Dinesh99673 <dc7821836954@gmail.com>
2025-05-08 16:22:08 +0530

    saving data format changed and also retrival mechanism added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 import tkinter
 from tkinter import messagebox
 import pyperclip
+import json
 
 window = tkinter.Tk()
 window.title("Password Manager")
@@ -37,18 +38,50 @@ def save_data():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {website:{
+        "email":email,
+        "password":password
+    }}
 
     if len(website)==0 or len(password)==0:
         messagebox.showwarning(title="Ooops",message="Please dont't leave any fields empty :(")
         return    
     is_ok = messagebox.askokcancel(title=website,message=f"This are the details entered : \n Email :- {email} \n Password :- {password} \n Is it ok to save ?")
     if is_ok:
-        with open("data.txt", "a") as file:
-            file.write(f"{website} | {email} | {password}\n")
-            website_entry.delete(0,len(website))
-            password_entry.delete(0,len(password))
-            messagebox.showinfo(title="Success",message="Data saved successfully ")
+        try:
+            with open("data.json", "r") as file:
+                #Reading old data
+                data = json.load(file)
+                #Updating old data
+                data.update(new_data)
+        except:
+            data = new_data
+        with open("data.json","w") as file:
+            #saving the new data
+            json.dump(data,file, indent=4) # Dictionary data is saved in JSON format automatically
+        website_entry.delete(0,len(website))
+        password_entry.delete(0,len(password))
+        messagebox.showinfo(title="Success",message="Data saved successfully ")
 
+# ---------------------------- Searching ------------------------------- #
+
+def search():
+    print("In search")
+    website = website_entry.get()
+    try:
+        with open("data.json","r") as file:
+            data = json.load(file)
+    except:
+        messagebox.showwarning(title="Data not found", message="Something went Wrong, Data not found !!")
+    try:
+        found_data = data[website]
+        email = found_data["email"]
+        password = found_data["password"]
+        messagebox.showinfo(title="Found",message=f"Email : {email}\nPassword : {password}\n")
+    except:
+        messagebox.showinfo(title="Not Found",message=f"No data found for entered {website} website/doman name")
+    finally:
+        website_entry.delete(0,len(website))
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -69,7 +102,7 @@ password_label = tkinter.Label(text="Password :")
 password_label.grid(column=0, row=3)
 
 #Entries/Input Boxs
-website_entry = tkinter.Entry(width=42)
+website_entry = tkinter.Entry(width=36)
 website_entry.grid(row=1, column=1, columnspan=3)
 website_entry.focus()
 
@@ -83,6 +116,9 @@ password_entry.grid(column=1, row=3)
 #Buttons
 generate_password_button = tkinter.Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(column=2, row=3)
+
+search_button = tkinter.Button(text="Searh", command=search, width=10)
+search_button.grid(column=2, row=1)
 
 add_button = tkinter.Button(text="Add",width=36, command=save_data)
 add_button.grid(row=4, column=1, columnspan=2)
